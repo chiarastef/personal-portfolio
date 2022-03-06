1f63d20c178f99df49b8df616dbe0bd04eb2a7fe
Chiara Stefanelli <chiarastefanelli27@gmail.com>
2022-03-06 20:16:20 +0100

    Updated website

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,22 @@ const navLink = document.querySelectorAll(".nav-link");
 const navButton = document.querySelector(".navbar-toggler");
 const navbar = document.querySelector(".navbar-collapse");
 
+// Collapse navbar when user clicks on navbar links
 for (let i = 0; i < navLink.length; i++) {
-  navLink[i].addEventListener("click", function () {
-    navButton.classList.add("collapsed");
-    navButton.setAttribute("aria-expanded", "false");
-    navbar.classList.add("collapsed");
-    navbar.classList.remove("show");
-  });
+  navLink[i].addEventListener("click", collapseNav);
+}
+
+// Collapse navbar when user clicks outside of navbar
+document.addEventListener("click", function (event) {
+  if (navbar.classList.contains("show") && event.target != navbar) {
+    collapseNav();
+  }
+});
+
+// Collapse navbar
+function collapseNav() {
+  navButton.classList.add("collapsed");
+  navButton.setAttribute("aria-expanded", "false");
+  navbar.classList.add("collapsed");
+  navbar.classList.remove("show");
 }
