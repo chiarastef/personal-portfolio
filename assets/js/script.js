e700001a65be3bd0ad60591411166a065350a107
Chiara Stefanelli <chiarastefanelli27@gmail.com>
2022-11-03 20:41:55 +0200

    Added email form response

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ const navLink = document.querySelectorAll(".nav-link");
 const navButton = document.querySelector(".navbar-toggler");
 const navbar = document.querySelector(".navbar-collapse");
 
+const emailForm = document.getElementById("contact-form");
+const inputs = document.querySelectorAll("#name, #email, #message");
+const emailSuccess = document.getElementById("email-sent");
+const newEmail = document.getElementById("new-email");
+
 // Collapse navbar when user clicks on navbar links
 for (let i = 0; i < navLink.length; i++) {
   navLink[i].addEventListener("click", collapseNav);
@@ -20,4 +25,22 @@ function collapseNav() {
   navButton.setAttribute("aria-expanded", "false");
   navbar.classList.add("collapsed");
   navbar.classList.remove("show");
+}
+
+// Hide email form when submit is successful
+function hideEmailForm() {
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+
+  emailForm.style.display = "none";
+  emailSuccess.style.display = "block";
+}
+
+// Show email form when button clicked
+newEmail.addEventListener("click", showEmailForm);
+
+function showEmailForm() {
+  emailForm.style.display = "block";
+  emailSuccess.style.display = "none";
 }
